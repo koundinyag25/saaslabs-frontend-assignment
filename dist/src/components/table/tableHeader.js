010611f4cdeabf9bc0b61604cedea7b27185b62c
koundinyag25 <koundinya1991@gmail.com>
2025-01-15 15:21:46 +0530

    fix

--- dist/src/components/table/tableHeader.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const TableHeader = ({ config }) => {
-    return (_jsx("thead", { className: "table-header", children: _jsx("tr", { children: config.map((item) => (_jsx("th", { children: item.label }, item.key))) }) }));
-};
-export default TableHeader;
--- dist/src/services/repos.js
+++
@@ -1,9 +0,0 @@
-export const getRepoList = async () => {
-    try {
-        const result = await fetch("https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json").then((res) => res.json());
-        return result;
-    }
-    catch (error) {
-        return error;
-    }
-};
